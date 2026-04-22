14f330262a5ed3081436e799ae375eecc12a60b0
Hezitzaile <juanmaria.diaz@opendeusto.es>
2026-04-22 16:23:26 +0200

    Añadimos la app a Settings.py y las urls de la app, con include para meter todas las urls de al app

--- Deusto_Plant_Operations/Deusto_Plant_Operations/settings.py
+++ Deusto_Plant_Operations/Deusto_Plant_Operations/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #Añado la aplicacion para poder ejecutarla.
+    'Gestion',
 ]
 
 MIDDLEWARE = [
--- Deusto_Plant_Operations/Deusto_Plant_Operations/urls.py
+++ Deusto_Plant_Operations/Deusto_Plant_Operations/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    #Añado el path de la app. Con include lo que hacemos es incluir todas las urls de la aplicacion sin necesidad de escribirlas aqui
+    path('', include('Gestion.urls'),)
 ]
